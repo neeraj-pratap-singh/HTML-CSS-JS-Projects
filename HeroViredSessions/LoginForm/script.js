2646d68ada89a4501d26eff7720e787089a721c3
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-10-07 15:11:03 +0530

    login form created

---
+++ HeroViredSessions/LoginForm/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+    
+        console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+    });
+});

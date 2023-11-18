f522fa46e476ce96e68c7f45a82aa40168027068
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-11-18 15:11:26 +0530

    updated button outside the div

--- HeroViredSessions/DOMManipulation/PractiseSession/index.js
+++ HeroViredSessions/DOMManipulation/PractiseSession/index.js
@@ -1,26 +1,21 @@
-// Click event
+// Select the div and button elements
 let mydiv = document.querySelector(".sample");
 let innerDivValue = 0;
+
+// Function to update the inner div content
+function updateInnerDiv() {
+  mydiv.innerHTML = `<div style='background-color: red; height: 50px; width: 50px;'>${innerDivValue}</div>`;
+}
+
+// Click event for the div
 mydiv.addEventListener("click", function () {
-  // alert("your div has been clicked")
-  // mydiv.style.color="red"
   mydiv.style.height = "200px";
   mydiv.style.width = "200px";
-  // on click sample1 text should be replaced with herovired
-  //    mydiv.innerText ="Herovired"
-  mydiv.innerHTML = `<div style='background-color: red;height: 50px;width: 50px;'>
-  ${innerDivValue}
-  </div>
-  
-  
-  <button id="btn">Increment</button>
-  `;
-  const btn = document.getElementById("btn");
-  console.log(btn);
-  btn.addEventListener("click", function () {
-    // alert("btn is clicked");
-    //update the counter logic
-    innerDivValue += 1
-  });
+  updateInnerDiv();
 });
-// btn should be outside of whole blue box ;
+
+// Select the button by ID and add click event
+document.getElementById("btn").addEventListener("click", function () {
+  innerDivValue += 1;
+  updateInnerDiv();
+});

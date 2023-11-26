73032b94582bdb6d42a1b1177f96fae3d5abfd44
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-11-26 15:21:03 +0530

    post request ajax call

--- HeroViredSessions/DOMManipulation/AjaxExample/script.js
+++ HeroViredSessions/DOMManipulation/AjaxExample/script.js
@@ -2,17 +2,30 @@
 console.log('API AJAX call')
 const result = document.getElementById('result');
 document.getElementById('ReqresData').addEventListener('click', function() {
-    // GET request
+    // // GET request
+    // var xhr = new XMLHttpRequest(); // create an object
+    // xhr.open('GET', 'https://reqres.in/api/users?page=2'); //Open the port
+    // xhr.send(); // send the request
+    // xhr.onload = function() {
+    //         var data = JSON.parse(xhr.responseText);
+    //         console.log('API response', data)
+    //         result.innerHTML = data.data;
+    // };
+
+    // POST request
     var xhr = new XMLHttpRequest(); // create an object
-    xhr.open('GET', 'https://reqres.in/api/users?page=2'); //Open the port
-    xhr.send(); // send the request
+    userData = {
+            name: "morpheus",
+            job: "leader"
+    }
+    JsonUserData = JSON.stringify(userData);
+    xhr.open('POST', 'https://reqres.in/api/users'); //Open the port
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send(JsonUserData); // send the request
     xhr.onload = function() {
             var data = JSON.parse(xhr.responseText);
             console.log('API response', data)
-            result.innerHTML = data.data;
     };
-
-    
 
 
 });
